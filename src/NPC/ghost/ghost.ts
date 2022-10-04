6671cac0838964d7e5345edc741ed7cabd6f372a
Alexey Knyazev <aleks.kn@gmail,com>
2022-10-04 21:52:37 +0400

    fix

--- src/NPC/ghost/ghost.ts
+++ src/NPC/ghost/ghost.ts
@@ -140,7 +140,7 @@ export class Ghost extends Entity {
             if (
               this.inCooldown ||
               sharedDialog.isDialogOpen ||
-              followingGhost == []
+              followingGhost.length == 0
             ) {
               return
             }
