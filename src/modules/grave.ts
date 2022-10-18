292acbda633bdba4cb6e4b7c8372ba0c846a80c0
Vladislav Rytov <60110864+PartyMar@users.noreply.github.com>
2022-10-18 18:02:27 +0300

    hide creating extra checkboxes

--- src/modules/grave.ts
+++ src/modules/grave.ts
@@ -48,7 +48,7 @@ export function doorHauntedHouse() {
           return
         }
         doorHauntedHouseAnim.play()
-        quest.checkBox(3)
+        //quest.checkBox(3)
         useKey()
         const source = new AudioSource(openClip)
         doorHauntedHouse.addComponentOrReplace(source)
@@ -468,8 +468,8 @@ export function day1GirlDialog(callback:()=>void){
     {
       text: `If you are listening to this, come to the Glass Pavilion, near the 0,0 coordinates, I believe you are the one who can help me.`,
       triggeredByNext: () => {
-        quest.checkBox(1)
-        quest.showCheckBox(2)
+        //quest.checkBox(1)
+        //quest.showCheckBox(2)
         updateProgression('phone')
 
         callback()
@@ -511,9 +511,9 @@ That’s why sometimes we gather around to dance.`,
     {
       text: `Move to the rhythm, bro!`,
       triggeredByNext: () => {
-        quest.checkBox(1)
-        quest.showCheckBox(2)
-        quest.showCheckBox(3)
+        //quest.checkBox(1)
+        //quest.showCheckBox(2)
+        //quest.showCheckBox(3)
         //updateProgression('ghostIntro')
         ghostUIBck.image.visible = true
         ghostCounter.uiText.visible = true
@@ -1340,7 +1340,7 @@ export function counterIncrease() {
   ghostCounter.increase()
   if (ghostCounter.read() >= 6) {
     // update quest info
-    quest.checkBox(2)
+    //quest.checkBox(2)
     updateProgression('ghostsDone')
     // remove UI
     ghostUIBck.image.visible = false
@@ -1457,16 +1457,6 @@ export function addGhostsAndCrypts() {
     'Trigger_Close'
   )
 
-  let crypt2 = new Grave(
-    {
-      position: new Vector3(81 + 8, 0.2, 15),
-      rotation: Quaternion.Euler(0, 90, 0), scale: new Vector3(1.35, 1.35, 1.35),
-    },
-    new GLTFShape(graveyard_models.machete),
-    null,
-    'Machete_Trigger',
-    'Trigger_Close'
-  )
 
   let crypt3 = new Grave(
     {
@@ -1487,16 +1477,6 @@ export function addGhostsAndCrypts() {
   )
   engine.addEntity(dummyForCat)
 
-  let crypt4 = new Grave(
-    {
-      position: new Vector3(21 + 8, 0, 16),
-      rotation: Quaternion.Euler(0, 270, 0),
-    },
-    new GLTFShape(graveyard_models.catCrypt),
-    null,
-    'Trigger',
-    'Trigger_Close'
-  )
 
   let crypt5 = new Grave(
     {
@@ -1520,17 +1500,6 @@ export function addGhostsAndCrypts() {
     'Trigger_Close'
   )
 
-  let crypt7 = new Grave(
-    {
-      position: new Vector3(35 + 8, 0, 66.3),
-      rotation: Quaternion.Euler(0, 180, 0),
-      scale: new Vector3(0.7, 0.7, 0.7)
-    },
-    new GLTFShape(graveyard_models.zombieHand),
-    null,
-    'ZombieHand_Trigger',
-    'Trigger_Close'
-  )
 
   let crypt8 = new Grave(
     {
@@ -1543,19 +1512,6 @@ export function addGhostsAndCrypts() {
     'Trigger_Close_Armature'
   )
 
-
-
-  let crypt10 = new Grave(
-    {
-      position: new Vector3(92 + 8, 0, 69),
-      rotation: Quaternion.Euler(0, 90, 0),
-      scale: new Vector3(0.7, 0.7, 0.7)
-    },
-    new GLTFShape(graveyard_models.zombieHand),
-    null,
-    'ZombieHand_Trigger',
-    'Trigger_Close'
-  )
 
   let crypt11 = new Grave(
     {
