a53634953f8c4d0eb8242430b597295e2080916b
dc-ankou <94530449+dc-ankou@users.noreply.github.com>
2022-10-20 13:27:10 +0300

    update

--- src/modules/grave.ts
+++ src/modules/grave.ts
@@ -838,7 +838,7 @@ let sharedDialogTimerEntity = new Entity()
 engine.addEntity(sharedDialogTimerEntity)
 
 let sharedDialog = new NPCUtils.DialogWindow(
-  { path: 'images/portraits/main-ghost.png' },
+  { path: 'images/portraits/skeleton.png' },
   true
 )
 sharedDialog.panel.height = 150
@@ -1238,7 +1238,7 @@ export function addMainGhostNPC(progression: HalloweenState) {
       //mainGhost.playAnimation(`Head_Yes`, true, 2.63)
     },
     {
-      portrait: { path: 'images/portraits/main-ghost.png' },
+      portrait: { path: 'images/portraits/skeleton.png' },
       reactDistance: 10,
       idleAnim: `idle1`
     },
@@ -1246,7 +1246,7 @@ export function addMainGhostNPC(progression: HalloweenState) {
   )
 
   mainGhost.dialog = new NPCUtils.DialogWindow(
-    { path: 'images/portraits/main-ghost.png' },
+    { path: 'images/portraits/skeleton.png' },
     true,
     '',
     halloweenTheme
