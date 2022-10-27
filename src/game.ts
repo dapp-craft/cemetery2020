fddb3d03872749ef3ab25da84bebc0249726d1ce
Alexey Knyazev <aleks.kn@gmail,com>
2022-10-27 12:43:29 +0300

    disable hotkeys

--- src/game.ts
+++ src/game.ts
@@ -29,26 +29,26 @@ export function setUpScene() {
 
   doorHauntedHouse()
 }
-
-Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (e) => {
-  log(
-    `new Vector3(`,
-    Camera.instance.position.x,
-    ',',
-    Camera.instance.position.y,
-    ',',
-    Camera.instance.position.z,
-    `),
-	rotation: Quaternion.Euler(`,
-    0,
-    ',',
-    Camera.instance.rotation.eulerAngles.y,
-    ',',
-    0,
-    `),
-    },`
-  )
-})
+//
+// Input.instance.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (e) => {
+//   log(
+//     `new Vector3(`,
+//     Camera.instance.position.x,
+//     ',',
+//     Camera.instance.position.y,
+//     ',',
+//     Camera.instance.position.z,
+//     `),
+// 	rotation: Quaternion.Euler(`,
+//     0,
+//     ',',
+//     Camera.instance.rotation.eulerAngles.y,
+//     ',',
+//     0,
+//     `),
+//     },`
+//   )
+// })
 
 function updateSceneByProgression() {
 
@@ -100,52 +100,52 @@ onSceneReadyObservable.add(() => {
 })
 
 
-const input = Input.instance
-
-const stages = [
-  'talkBat',
-  'meetGirl',
-
-  'allHouses',
-  'phone',
-  'pumpkinDone',
-  'w1Found',
-
-  'NPCIntroDay2',
-  // 'ghostsDone',
-  // 'w2Found'
-]
-
-let stage_index = 0
-input.subscribe('BUTTON_DOWN', ActionButton.ACTION_3, false, (e) => {
-  // log('pointer Down', e)
-  log('updateProgression', stages[stage_index])
-  if (updateProgression(stages[stage_index])) {
-    log('updateProgression success', stages[stage_index])
-    stage_index++
-  }
-})
-
-input.subscribe('BUTTON_DOWN', ActionButton.ACTION_4, false, (e) => {
-  const rewardtest = new Entity()
-
-  rewardtest.addComponent(new Transform({ position: new Vector3(115, 1.05, 45.83) }))
-  engine.addEntity(rewardtest)
-
-  let r = new Reward(
-    rewardtest,
-    'w2',
-    {
-      position: new Vector3(0.2, 0.8, 0.5),
-      scale: new Vector3(2, 2, 2) 
-    },
-    false,
-    () => {
-      executeTask(async () => {
-        await updateProgression('w2Found')
-        progression.data['w2Found'] = true
-        await nextDay(3)
-      })
-    }
-  )
-})
+// const input = Input.instance
+//
+// const stages = [
+//   'talkBat',
+//   'meetGirl',
+//
+//   'allHouses',
+//   'phone',
+//   'pumpkinDone',
+//   'w1Found',
+//
+//   'NPCIntroDay2',
+//   // 'ghostsDone',
+//   // 'w2Found'
+// ]
+//
+// let stage_index = 0
+// input.subscribe('BUTTON_DOWN', ActionButton.ACTION_3, false, (e) => {
+//   // log('pointer Down', e)
+//   log('updateProgression', stages[stage_index])
+//   if (updateProgression(stages[stage_index])) {
+//     log('updateProgression success', stages[stage_index])
+//     stage_index++
+//   }
+// })
+//
+// input.subscribe('BUTTON_DOWN', ActionButton.ACTION_4, false, (e) => {
+//   const rewardtest = new Entity()
+//
+//   rewardtest.addComponent(new Transform({ position: new Vector3(115, 1.05, 45.83) }))
+//   engine.addEntity(rewardtest)
+//
+//   let r = new Reward(
+//     rewardtest,
+//     'w2',
+//     {
+//       position: new Vector3(0.2, 0.8, 0.5),
+//       scale: new Vector3(2, 2, 2)
+//     },
+//     false,
+//     () => {
+//       executeTask(async () => {
+//         await updateProgression('w2Found')
+//         progression.data['w2Found'] = true
+//         await nextDay(3)
+//       })
+//     }
+//   )
+// })
